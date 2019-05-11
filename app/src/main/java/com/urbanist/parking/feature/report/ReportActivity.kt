7e6f739987bbde5429497eb4e08fa73bb0f4a618
seva <szhukov@icerockdev.com>
2019-05-11 17:05:14 +0700

    загрузка фото в imageview

--- app/src/main/java/com/urbanist/parking/feature/report/ReportActivity.kt
+++ app/src/main/java/com/urbanist/parking/feature/report/ReportActivity.kt
@@ -1,12 +1,15 @@
 package com.urbanist.parking.feature.report
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import com.urbanist.parking.R
 import com.urbanist.parking.core.presentation.BaseActivity
@@ -14,12 +17,15 @@ import com.urbanist.parking.databinding.ActivityReportBinding
 import javax.inject.Inject
 import android.view.MenuItem
 import com.urbanist.parking.feature.recomendation.RecommendationActivity
+import com.urbanist.parking.utils.hasNull
 import kotlinx.android.synthetic.main.activity_report.*
 
 class ReportActivity : BaseActivity<ActivityReportBinding>() {
 
-	var isLocationEnabled = true
-	var currentLocation = Location(LocationManager.GPS_PROVIDER)
+	private var isLocationEnabled = true
+	private var currentLocation = Location(LocationManager.GPS_PROVIDER)
+	private var currentPhotoId = 0
+	private var images: Array<Bitmap?> = arrayOfNulls(3)
 
 	@Inject
 	lateinit var viewModel: ReportViewModel
@@ -32,6 +38,13 @@ class ReportActivity : BaseActivity<ActivityReportBinding>() {
 
 	override fun initViewModel(state: Bundle?) {
 		viewModel.onBind()
+		viewModel.setEventListener(eventsListener)
+	}
+
+	private val eventsListener: ReportViewModel.EventsListener = object : ReportViewModel.EventsListener {
+		override fun getPhoto(i: Int) {
+			openCamera(i)
+		}
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,129 +87,30 @@ class ReportActivity : BaseActivity<ActivityReportBinding>() {
 		menuInflater.inflate(R.menu.report_menu, menu)
 		return true
 	}
-/*
 
-    private var thumbnail: Bitmap? = null
-    private var thumbnailSecond: Bitmap? = null
-    private var thumbnailThird: Bitmap? = null
-    private var currentPhoto = 0
-    private lateinit var location: Location
+	private fun openCamera(numPhoto: Int) {
+		currentPhotoId = numPhoto
+		val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+		startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST)
+	}
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.report_menu, menu)
-        return true
-    }
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		when (requestCode) {
+			CAMERA_PIC_REQUEST -> if (resultCode == Activity.RESULT_OK) {
+				val bitmap = data?.extras?.get(DATA_KEY) as Bitmap
+				images[currentPhotoId] = bitmap
+				viewModel.setBitmapToImageView(currentPhotoId, bitmap)
+				sendButton.isEnabled = images.hasNull().not()
+			}
+		}
+	}
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        startActivity(Intent(this, RecommendationActivity::class.java))
-        return super.onOptionsItemSelected(item)
-    }
+	companion object {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        AppComponent.instance.inject(this)
-        setContentView(R.layout.activity_report)
-        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-        imageViewSecond.scaleType = ImageView.ScaleType.CENTER_CROP
-        imageViewThird.scaleType = ImageView.ScaleType.CENTER_CROP
-        imageView.setOnClickListener {
-            openCamera(0)
-        }
-        imageViewSecond.setOnClickListener {
-            openCamera(ic_icon)
-        }
-        imageViewThird.setOnClickListener {
-            openCamera(ic_report_photo_1)
-        }
-        sendButton.setOnClickListener {
-            sendReport()
-        }
-    }
-
-    private fun openCamera(numPhoto: Int) {
-        currentPhoto = numPhoto
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-            CAMERA_PIC_REQUEST -> if (resultCode == Activity.RESULT_OK) {
-                val bitmap = data?.extras?.get("data") as Bitmap
-                when (currentPhoto) {
-                    0 -> thumbnail = bitmap
-                    ic_icon -> thumbnailSecond = bitmap
-                    ic_report_photo_1 -> thumbnailThird = bitmap
-                }
-                imageView.setImageBitmap(thumbnail)
-                imageViewSecond.setImageBitmap(thumbnailSecond)
-                imageViewThird.setImageBitmap(thumbnailThird)
-                sendButton.isEnabled = thumbnail != null && thumbnailSecond != null && thumbnailThird != null
-            }
-        }
-    }
-
-    private fun sendReport() {
-        val locationResult = object : LocationResult() {
-            override fun gotLocation(location: Location) {
-                this@ReportActivity.location = location
-            }
-        }
-        val myLocation = MyLocation()
-        myLocation.getLocation(this, locationResult)
-        sendPhoto(thumbnail!!)
-        sendPhoto(thumbnailSecond!!)
-        sendPhoto(thumbnailThird!!)
-        val url1 =  "$thumbnail"
-        val url2 =  "$thumbnailSecond"
-        val url3 =  "$thumbnailThird"
-        val report = hashMapOf<String, String>()
-        report["image_name_1"] = url1
-        report["image_name_2"] = url2
-        report["image_name_3"] = url3
-        report["comment"] = comment.text.toString()
-        if (::location.isInitialized) {
-            report["location"] = "${location.latitude}:${location.longitude}"
-        } else {
-            Toast.makeText(this, "Включите определение местоположения", Toast.LENGTH_SHORT).show()
-            return
-        }
-        progressBar.visibility = View.VISIBLE
-        sendButton.isEnabled = false
-        fireStore.collection("reports")
-            .add(report)
-            .addOnSuccessListener { documentReference ->
-                progressBar.visibility = View.GONE
-                Toast.makeText(this, "Репорт успешно отпрвлен!", Toast.LENGTH_SHORT).show()
-                imageView.setImageResource(0)
-                imageViewSecond.setImageResource(0)
-                imageViewThird.setImageResource(0)
-                thumbnail = null
-                thumbnailSecond = null
-                thumbnailThird = null
-                comment.setText("")
-                Log.d("kek", "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
-                sendButton.isEnabled = true
-                Log.w("kek", "Error adding document", e)
-            }
-    }
-
-    private fun sendPhoto(bitmap: Bitmap) {
-        val outputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-        val data = outputStream.toByteArray()
-        val path = fireStorage.reference.child("$bitmap")
-        path.putBytes(data)
-    }
-
-
-    companion object {
-        const val CAMERA_PIC_REQUEST = 20000
-    }*/
+		const val CAMERA_PIC_REQUEST = 20000
+		const val DATA_KEY = "data"
+	}
 
 	override fun onDestroy() {
 		super.onDestroy()
--- app/src/main/java/com/urbanist/parking/feature/report/ReportViewModel.kt
+++ app/src/main/java/com/urbanist/parking/feature/report/ReportViewModel.kt
@@ -1,8 +1,56 @@
 package com.urbanist.parking.feature.report
 
+import android.graphics.Bitmap
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import com.urbanist.parking.core.presentation.BaseViewModel
 import javax.inject.Inject
 
 class ReportViewModel @Inject constructor() : BaseViewModel() {
 
+	val firstPhoto = MutableLiveData<Bitmap?>()
+	val secondPhoto = MutableLiveData<Bitmap?>()
+	val thirdPhoto = MutableLiveData<Bitmap?>()
+
+	val sendButtonEnable = MediatorLiveData<Boolean>()
+
+	lateinit var eventsListener: EventsListener
+
+	init {
+		firstPhoto.value = null
+		secondPhoto.value = null
+		thirdPhoto.value = null
+
+		sendButtonEnable.apply {
+			addSource(firstPhoto) { it != null }
+			addSource(secondPhoto) { it != null }
+			addSource(thirdPhoto) { it != null }
+		}
+	}
+
+	fun setBitmapToImageView(currentPhotoId: Int, bitmap: Bitmap) {
+		when (currentPhotoId) {
+			FIRST_PHOTO_ID -> firstPhoto.value = bitmap
+			SECOND_PHOTO_ID -> secondPhoto.value = bitmap
+			THIRD_PHOTO_ID -> thirdPhoto.value = bitmap
+		}
+	}
+
+	fun onPhotoClick(i: Int) {
+		eventsListener.getPhoto(i)
+	}
+
+	fun setEventListener(eventsListener: EventsListener) {
+		this.eventsListener = eventsListener
+	}
+
+	interface EventsListener {
+		fun getPhoto(i: Int)
+	}
+
+	companion object {
+		const val FIRST_PHOTO_ID = 0
+		const val SECOND_PHOTO_ID = 1
+		const val THIRD_PHOTO_ID = 2
+	}
 }
