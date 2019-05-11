92d85f94d5d5cab64638e91040fb5b3f415154c7
seva <szhukov@icerockdev.com>
2019-05-11 14:13:05 +0700

    запрос разрешений на определение геолокации

--- app/src/main/java/com/urbanist/parking/feature/main/MainActivity.kt
+++ app/src/main/java/com/urbanist/parking/feature/main/MainActivity.kt
@@ -13,37 +13,48 @@ import com.urbanist.parking.R
 import com.urbanist.parking.feature.report.ReportActivity
 import com.urbanist.parking.feature.rules.RulesActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import com.urbanist.parking.feature.main.location_denied.LocationDeniedDialog
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        rulesButton.setOnClickListener {
-            startActivity(Intent(this, RulesActivity::class.java))
-        }
-        reportButton.setOnClickListener {
-            requestGeoAndCameraPermissions()
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		rulesButton.setOnClickListener {
+			startActivity(Intent(this, RulesActivity::class.java))
+		}
+		reportButton.setOnClickListener {
+			requestGeoAndCameraPermissions()
+		}
+	}
 
-    private fun requestGeoAndCameraPermissions() {
-        Dexter.withActivity(this)
-            .withPermissions(
-                Manifest.permission.CAMERA,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ).withListener(object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
-                    startActivity(Intent(this@MainActivity, ReportActivity::class.java))
-                }
+	private fun requestGeoAndCameraPermissions() {
+		Dexter.withActivity(this)
+			.withPermissions(
+				Manifest.permission.CAMERA,
+				Manifest.permission.ACCESS_FINE_LOCATION,
+				Manifest.permission.ACCESS_COARSE_LOCATION
+			).withListener(object : MultiplePermissionsListener {
+				override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+					if (report.isAnyPermissionPermanentlyDenied) {
+						showDeniedLocationDialog()
+					} else {
+						startActivity(Intent(this@MainActivity, ReportActivity::class.java))
+					}
+				}
 
-                override fun onPermissionRationaleShouldBeShown(
-                    permissions: List<PermissionRequest>,
-                    token: PermissionToken
-                ) {
+				override fun onPermissionRationaleShouldBeShown(
+					permissions: List<PermissionRequest>,
+					token: PermissionToken
+				) {
+					token.continuePermissionRequest()
+				}
+			}).withErrorListener {
+				showDeniedLocationDialog()
+			}.check()
+	}
 
-                }
-            }).check()
-    }
+	private fun showDeniedLocationDialog() {
+		LocationDeniedDialog(this@MainActivity).show()
+	}
 }
