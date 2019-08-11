b68d9c527e8a55d7fdf07288a8f6743e00726352
seva <szhukov@icerockdev.com>
2019-08-12 01:20:07 +0700

    dwefwrf

--- app/src/main/java/com/urbanist/parking/core/dagger/scope/ApplicationScope.kt
+++ app/src/main/java/com/urbanist/parking/core/dagger/scope/ApplicationScope.kt
@@ -4,8 +4,4 @@ import javax.inject.Scope
 
 @Scope
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-annotation class ActivityScope
-
-@Scope
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class ApplicationScope
